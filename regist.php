009eeda0365ebe144e30eecb729d5496f3db387a
github FuyukiTaniguchi <valance46@yahoo.co.jp>
2021-07-13 20:19:10 +0900

    commit

---
+++ regist.php
@@ -0,0 +1,222 @@
+<?php
+require('./dbconnect.php');
+session_start();
+
+$spaceExistence = '^(\s|　)+$';
+$reg_str = '/^[^ ]+@[^ ]+\.[a-z]{2,3}$/';
+
+
+
+
+//submit入力時コードを走らせる
+if (!empty($_POST)) {
+    $rewrite = $_POST;
+
+    if ($_POST['user'] == '' || mb_ereg_match($spaceExistence, $_POST['user'])) {
+        $error['user'] = 'blank';
+    }
+
+    if ($_POST['mail'] == '' || mb_ereg_match($spaceExistence, $_POST['mail']) || !preg_match($reg_str, rtrim($_POST['mail']))) {
+        $error['mail'] = 'blank';
+    }
+
+    if (mb_strlen($_POST['password']) < 4 || mb_ereg_match($spaceExistence, $_POST['password'])) {
+        $error['password'] = 'blank';
+    }
+
+    if (empty($error)) {
+        $member = $db->query('SELECT user , mail FROM members');
+        while ($members = $member->fetch()) {
+            if ($members['user'] === $_POST['user']) {
+                $error['same_user'] = 'same';
+            }
+
+            if ($members['mail'] === $_POST['mail']) {
+                $error['same_mail'] = 'same';
+            }
+        }
+    }
+
+    if (empty($error)) {
+        $_SESSION['join'] = $_POST;
+        header('location:confirm.php');
+        exit();
+    }
+}
+
+
+//書き直し時に値をvalue属性に送る
+if ($_REQUEST['action'] == 'rewrite') {
+    $rewrite = $_SESSION['join'];
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        .container {
+            max-width: 392px;
+        }
+
+
+        body {
+            height: 100%;
+            background: url(image/daniel-schludi-mbGxz7pt0jM-unsplash.jpg);
+            backdrop-filter: sepia(90%);
+            -webkit-backdrop-filter: sepia(100%);
+        }
+
+        .login {
+            padding-top: 12rem;
+        }
+
+        .visibility_hidden {
+            height: 24px;
+            color: #00f915;
+        }
+
+        .input_area {
+            padding: 35px 16px 8px;
+            background-color: #a78b8b;
+            opacity: 0.8;
+        }
+
+        form {
+            position: relative;
+        }
+
+        .input_area input,
+        a {
+            width: 392px;
+            padding: 0.5rem;
+            transition: all .6s ease;
+
+            border: none;
+            outline: none;
+        }
+
+        .regist {
+            /* background-color: #d86826; */
+            background-color: #d39e00;
+            color: #f3e5e5;
+            border-radius: 50px;
+
+        }
+
+        .return {
+            background-color: #a5a992;
+            color: #f3e5e5;
+            border-radius: 50px;
+        }
+
+        #form.mail_valid .mail_area:before {
+            content: '';
+            position: absolute;
+            right: 0.9px;
+            top: 9.4em;
+            width: 24px;
+            height: 24px;
+            background: url(image/valid.png);
+            background-size: cover;
+
+        }
+
+        #form.pass_valid .pass_area:before {
+            content: '';
+            position: absolute;
+            right: 1px;
+            top: 16em;
+            width: 24px;
+            height: 24px;
+            background: url(image/valid.png);
+            background-size: cover;
+
+        }
+    </style>
+</head>
+
+<body class="bg-light">
+    <div id="wrapper ">
+        <div class="container login">
+            <form id="form" action="" method="post" enctype="multipart/form-data">
+                <div class="row justify-content-center">
+                    <div class="input_area ">
+                        <input type="text" name="user" placeholder="ユーザーネーム" maxlength="20" value="<?php if ($rewrite) echo htmlspecialchars($rewrite['user'], ENT_QUOTES) ?>">
+                        <span class="visibility_hidden d-block"><?php if ($error['same_user'] === 'same') : echo 'そのユーザ名は存在してます';
+                                                                endif; ?><?php if ($error['user'] === 'blank') : echo 'ユーザ名を入力してください';
+                                                                            endif; ?></span>
+                    </div>
+                    <div class="input_area mail_area">
+                        <input type="text" id="mail" name="mail" placeholder="メールアドレス" maxlength="50" value="<?php echo htmlspecialchars($rewrite['mail'], ENT_QUOTES) ?>" onkeyup="checkMail()">
+                        <span id="check_email" class="d-block visibility_hidden"><?php if ($error['same_mail'] === 'same') : echo 'そのメールアドレスは存在してます';
+                                                                                    endif;
+                                                                                    if ($error['mail'] === 'blank') : echo 'メールアドレスを入力してください';
+                                                                                    endif; ?></span>
+                    </div>
+                    <div class="input_area pass_area">
+                        <input type="password" id="password" name="password" placeholder="パスワード" onkeyup="checkPassword()">
+                        <span id="check_password" class="d-block visibility_hidden"><?php if ($error['password'] === 'blank') : echo 'パスワードを入力してください';
+                                                                                    endif; ?></span>
+                    </div>
+                    <div class="input_area ">
+                        <input type="submit" class="btn  text-center p-3 regist" value="新規登録"></button>
+                    </div>
+                    <div class="input_area ">
+                        <a href="top.php" class="btn  text-center p-3 return">戻る</a>
+                    </div>
+            </form>
+        </div>
+    </div>
+    </div>
+
+    <script>
+        const form = document.getElementById('form');
+
+        const checkMail = () => {
+            const email = document.getElementById('mail').value.trim();
+            const check_email = document.getElementById('check_email');
+            const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+            if (email.match(pattern)) {
+                form.classList.add('mail_valid');
+                check_email.innerHTML = '';
+            } else {
+                form.classList.remove('mail_valid');
+                check_email.innerHTML = '正しいメールアドレスを入力してください';
+            }
+
+            if (email == '') {
+                form.classList.remove('mail_valid');
+                check_email.innerHTML = '';
+            }
+        }
+
+        const checkPassword = () => {
+            let check_password = document.getElementById('check_password');
+            let password = document.getElementById('password').value.trim().length;
+
+            if (password >= 4) {
+                form.classList.add('pass_valid');
+                check_password.innerHTML = '';
+            } else {
+                form.classList.remove('pass_valid');
+                check_password.innerHTML = '4文字以上で入力してください';
+
+            }
+
+            if (password == 0) {
+                check_password.innerHTML = '';
+            }
+        }
+    </script>
+</body>
+
+</html>
+
+
---
+++ top.php
@@ -0,0 +1,173 @@
+<?php
+require('./dbconnect.php');
+session_start();
+
+if ($_POST['user'] !== null && $_POST['password'] !== null) {
+    $login = $db->prepare('SELECT * FROM members WHERE user=? AND password=? OR mail=? AND password=?');
+    $reg_str = '/^[^ ]+@[^ ]+\.[a-z]{2,3}$/';
+    //メールアドレスかユーザ名かでDBに渡す値を分岐する
+    if (preg_match($reg_str, rtrim($_POST['user']))) {
+        $login->execute(array('', sha1($_POST['password']), $_POST['user'], sha1($_POST['password'])));
+    } else {
+        $login->execute(array($_POST['user'], sha1($_POST['password']), '', sha1($_POST['password'])));
+    }
+    $user = $login->fetch();
+
+    if ($user) {
+        $_SESSION['id'] = $user['id'];
+        $_SESSION['user'] = $user['user'];
+        $_SESSION['time'] = time();
+        header('location:index.php');
+        exit();
+    } else {
+        $error['login'] = 'failed';
+    }
+}
+
+
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <style>
+        .container {
+            max-width: 432px;
+        }
+
+
+        body {
+            height: 100%;
+
+            background: url(image/daniel-schludi-mbGxz7pt0jM-unsplash.jpg);
+            backdrop-filter: sepia(100%);
+            -webkit-backdrop-filter: sepia(100%);
+        }
+
+        .heading {
+            color: #f4f4f4;
+            margin-top: 16rem;
+            font-size: 20px;
+        }
+
+        .acount {
+            background-color: #d39e00;
+            color: #f3e5e5;
+        }
+
+        .btn {
+            border-radius: 20px;
+        }
+
+        .input_area {
+            padding: 35px 16px 8px;
+        }
+
+        .input_area input,
+        a {
+            width: 392px;
+            padding: 0.5rem;
+            transition: all .6s ease;
+            border: none;
+            outline: none;
+        }
+
+        #login {
+            background-color: #d39e00;
+            color: #f3e5e5;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <p class="heading text-center">アーティスト、クリエイターが一番に聞かせたい<br>瞬間を投稿するサウンドアプリです。</p>
+                <a href="regist.php" class="btn col-lg-12 mb-4 acount">アカウント作成</a>
+                <button type="button" class="btn btn-light col-lg-12" data-toggle="modal" data-target="#Modal">ログイン</button>
+                <form action="" id="login_form" method="post">
+                    <div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="Modal" aria-hidden="true">
+                        <div class="modal-dialog modal-md" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title col-lg-12 text-center" id="Modal">ログインする</h5>
+                                </div>
+                                <div class="modal-body">
+                                    <?php if ($error['login'] === 'failed') : ?>
+                                        <span class="col-lg-12 text-center" id="valid">入力されたユーザー名やパスワードが正しくありません。</span>
+                                    <?php endif; ?>
+                                    <div class="input_area col-lg-12 text-center">
+                                        <input type="text" class="border border-primary" name="user" placeholder="メールアドレスまたはユーザーネーム" maxlength="50">
+                                    </div>
+                                    <div class="input_area col-lg-12 text-center">
+                                        <input type="password" class="border border-primary" name="password" placeholder="パスワード">
+                                    </div>
+                                </div>
+                                <div class="modal-footer ">
+                                    <button type="submit" class="btn" id="login">ログインする</button>
+                                </div>
+                            </div>
+                        </div>
+                </form>
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    <script>
+        const form = document.getElementById('login_form');
+
+        form.addEventListener('submit', (e) => {
+            // e.preventDefault();
+            const postData = new FormData(document.forms.login_form);
+            for (let setPost of postData) {
+                postData.set(setPost[0], setPost[1]);
+                console.log(setPost);
+            }
+
+            const data = {
+                method: 'post',
+                body: postData
+            };
+
+            fetch('top.php', data)
+                .then((res) => {
+                    if (res.status !== 200) {
+                        throw new Error("system error.");
+                    }
+                    return res.text();
+                }).then((text) => {
+                    console.log(text);
+                    // location.href = 'main.php'
+                }).catch((e) => {
+                    console.log(e.message);
+                }).finally(() => {
+                    console.log('done!')
+
+                });
+        });
+    </script>
+
+</html>
+</body>
